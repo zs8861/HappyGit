c7f8701bd41fc5c0965037f665ef2f6bae031cbf
zs8861 <117711668@qq.com>
2017-12-23 10:18:14 +0800

    add time

--- myWeather.py
+++ myWeather.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import datetime
+import time
 
 #token = hEeNABksrcQXVf8l
 #我家坐标: 120.130467,29.316559
@@ -9,6 +10,7 @@ import datetime
 res = requests.get("https://api.caiyunapp.com/v2/hEeNABksrcQXVf8l/120.130467,29.316559/realtime.json")
 myHome = json.loads(str(res.content, encoding = "utf-8"))
 
+server_time = myHome["server_time"] #服务器时间
 status = myHome["status"] #json返回状态
 temperature = myHome["result"]["temperature"] #温度
 pm25 = myHome["result"]["pm25"] #pm2.5
@@ -30,7 +32,7 @@ if status == "ok":
     elif skycon == "RAIN":
         skycon_zh = "有雨"
     elif skycon == "SNOW":
-        skycon_zh = "下雪"
+        skycon_zh = "下雪了"
     elif skycon == "WIND":
         skycon_zh = "有风"
     elif skycon == "FOG":
@@ -40,13 +42,16 @@ if status == "ok":
 else:
     print("获取天气失败！")
 
-time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+timeOnPC = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+timeOnServer = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(server_time))
 
 weather = str("东洲花园秋萍苑12幢上空, 当前气温: " + str(temperature) + 
               "度, 天气: " + str(skycon_zh) +
               ", PM2.5指数: " + str(pm25) +
-              ", 风速: " + str(wind_speed) + ", ")
-print(weather)   
+              ", 风速: " + str(wind_speed) + 
+              ", 天气服务器时间: " + str(timeOnServer) +
+              ", PC端时间: " + timeOnPC)
+print(weather)
 
-url = str("https://sc.ftqq.com/SCU17060Tcb6d837157a6f37b5789bbaa23979b715a1fd9926649c.send?text=" + "无邪的天气小秘书" + "&desp=" + weather + "时间: " + time_now)
+url = str("https://sc.ftqq.com/SCU17060Tcb6d837157a6f37b5789bbaa23979b715a1fd9926649c.send?text=" + "无邪的天气小秘书" + "&desp=" + weather)
 requests.get(url)
--- py4.py
+++ py4.py
@@ -1,52 +1,7 @@
-import json
+# -*- coding: utf-8 -*-
+import time
 
-'''
-data1 = {'b':789,'c':456,'a':123}
-data2 = {'a':123,'b':789,'c':456}
-d1 = json.dumps(data1,sort_keys=True)
-d2 = json.dumps(data2)
-d3 = json.dumps(data2,sort_keys=True)
-print(d1)
-print(d2)
-print(d3)
-print(d1==d2)
-'''
+time1 = time.time()
+print(time1)
 
-data3 = {
-    "status":"ok",
-    "lang":"zh_CN",  #目前只支持简体中文（zh_CN、zh_SG）、繁体中文（zh_TW、zh_HK），英语（en_US、en_GB）在测试中
-    "server_time":1443418222,
-    "tzshift":28800, #时区的偏移秒数，如东八区就是 28800 秒，使用秒是为了支持像尼泊尔这样的差 5 小时 45 分钟的地区，它们有非整齐的偏移量
-    "location":[
-        25.1552, #纬度
-        121.6544 #经度
-    ],
-    "unit":"metric", #目前只支持米制（metric）和科学计量法（SI），英制还有待开发
-    "result":{
-        "status":"ok",
-        "temperature":28.0,  #温度
-        "skycon":"RAIN",  #天气概况
-        "pm25": 11,       #pm25值   在新的api中增加的字段
-        "cloudrate":0.51,  # 云量
-        "humidity":0.92,  #相对湿度
-        "precipitation":{  #降水
-             "nearest":{ #最近的降水带 //用户补充：nearest这段有时候没有
-                 "status":"ok",
-                 "distance":0.77, #距离
-                 "intensity":0.3125 #角度
-             },
-            "local":{ #本地的降水
-                "status":"ok",
-                "intensity":0.2812, #降水强度
-                "datasource":"radar" #数据源
-            }
-        },
-        "wind":{ #风
-            "direction":25.33, #风向。单位是度。正北方向为0度，顺时针增加到360度。
-            "speed":83.3 #风速，米制下是公里每小时
-        }
-    }
-}
-
-#print(data3)
-print(data3["server_time"])
+print(time.localtime(time.time()))
