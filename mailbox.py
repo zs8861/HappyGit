ab4559d6661c4c8da9fc09ad44aa22f190ca3ca1
zs8861 <117711668@qq.com>
2017-12-29 07:45:04 +0800

    delete files

--- mailbox.py
+++ mailbox.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import re 
-=======
 #coding=utf-8
 import re         #正则表达式(regular expression)模块
 
@@ -9,7 +6,6 @@ import re         #正则表达式(regular expression)模块
 #\@    a\@b     a@b   (字符转义)
 #\.    a\.b     a.b   (字符转义)
 #这是个测试文件
->>>>>>> c2efc9a4b96a3486cfaaa18638fbebb5469b50bc
 
 def emails(e):
     if len(e)>= 5:
@@ -17,18 +13,7 @@ def emails(e):
             return '邮箱格式正确！'
     return '邮箱格式有误'
 
-<<<<<<< HEAD
 mail = input("请输入email: ")
 print(mail)
 a = emails(mail)
-=======
-ss = "zhengshuo"
-
-s1 = input("请输入email: ")
-print(s1)
-print(s1)
-a = emails(s1)
-
-print(a)
->>>>>>> c2efc9a4b96a3486cfaaa18638fbebb5469b50bc
 print(a)
--- py4.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import time
-
-time1 = time.time()
-print(time1)
-
-print(time.localtime(time.time()))
